2271632ab55c26f31965007b651108b93d6c1e0d
GeorgioWan <georgio_sky2000@hotmail.com>
2017-06-13 06:15:29 +0000

    update tests

--- tests/index-test.js
+++ tests/index-test.js
@@ -17,25 +17,49 @@ describe('Ocard', () => {
   
   it('Ocard test', () => {
     render(<Ocard/>, node, () => {
-      expect(node.innerHTML).toContain('<div data-reactroot="" id="oacard"><div id="oac-cover"><div class="oac-cover-img" style="background-image: url(http://0.0.0.0:8080/context.html);"></div><div class="oac-cover-gradient-left"></div><div class="oac-cover-filter"></div><div class="oac-cover-gradient"></div><!-- react-text: 7 --><!-- /react-text --></div><div id="wrapper"><div style="background-color: transparent; -webkit-transform: translate(0px, 0px) translateZ(0px); background-position: initial initial; background-repeat: initial initial;"><div id="oac-card-title-cover"><div id="oac-title" style="opacity: 1;"><h1>Main Title</h1><p>Sub Title...</p><span class="fa fa-angle-up fa-2x"></span></div></div><div id="oac-cards"><div class="oac-card"><div class="oac-card-table"><div class="oac-card-table-cell"><div class="oac-card-cover"><div class="oac-card-img" style="background-image: url(http://0.0.0.0:8080/context.html);"><div class="oac-card-img-cover" style="opacity: 0;"></div></div><div class="oac-card-triangle"></div></div><div class="oac-card-info"><h2>Card Title</h2><p>About this card...</p></div><div class="clr"></div></div><!-- react-text: 27 --><!-- /react-text --></div></div></div></div></div></div>')
-    })
-  })
+      expect(node.firstChild.id).toContain(`oacard`);
+    });
+  });
   
   it('OcardCover test', () => {
     render(<OcardCover/>, node, () => {
-      expect(node.innerHTML).toContain('<div data-reactroot="" id="oac-cover"><div class="oac-cover-img" style="background-image: url(http://0.0.0.0:8080/context.html);"></div><div class="oac-cover-gradient-left"></div><div class="oac-cover-filter"></div><div class="oac-cover-gradient"></div><!-- react-text: 6 --><!-- /react-text --></div>' )
-    })
-  })
+      let child = node.firstChild;
+      expect(child.id).toContain(`oac-cover`);
+      expect(child.children[0].className).toContain(`oac-cover-img`);
+      expect(child.children[1].className).toContain(`oac-cover-gradient-left`);
+      expect(child.children[2].className).toContain(`oac-cover-filter`);
+      expect(child.children[3].className).toContain(`oac-cover-gradient`);
+    });
+  });
   
   it('OcardCards test', () => {
     render(<OcardCards/>, node, () => {
-      expect(node.innerHTML).toContain('<div data-reactroot="" id="wrapper"><div style="background-color: transparent; -webkit-transform: translate(0px, 0px) translateZ(0px); background-position: initial initial; background-repeat: initial initial;"><div id="oac-card-title-cover"><div id="oac-title" style="opacity: 1;"><h1>Main Title</h1><p>Sub Title...</p><span class="fa fa-angle-up fa-2x"></span></div></div><div id="oac-cards"><div class="oac-card"><div class="oac-card-table"><div class="oac-card-table-cell"><div class="oac-card-cover"><div class="oac-card-img" style="background-image: url(http://0.0.0.0:8080/context.html);"><div class="oac-card-img-cover" style="opacity: 0;"></div></div><div class="oac-card-triangle"></div></div><div class="oac-card-info"><h2>Card Title</h2><p>About this card...</p></div><div class="clr"></div></div><!-- react-text: 20 --><!-- /react-text --></div></div></div></div></div>' )
+      let child = node.firstChild;
+      expect(child.id).toContain(`wrapper`);
     })
   })
   
   it('OcardCard test', () => {
     render(<OcardCard/>, node, () => {
-      expect(node.innerHTML).toContain('<div data-reactroot="" class="oac-card"><div class="oac-card-table"><div class="oac-card-table-cell"><div class="oac-card-cover"><div class="oac-card-img" style="background-image: url(http://0.0.0.0:8080/context.html);"><div class="oac-card-img-cover" style="opacity: 1;"></div></div><div class="oac-card-triangle"></div></div><div class="oac-card-info"><h2>Card Title</h2><p>About this card...</p></div><div class="clr"></div></div><!-- react-text: 12 --><!-- /react-text --></div></div>' )
+      
+      // <div class="oac-card">...</div>
+      let child = node.firstChild;
+      expect(child.className).toContain(`oac-card`);
+      
+      // <div class="oac-card-table">...</div>
+      child = child.firstChild;
+      expect(child.className).toContain(`oac-card-table`);
+      
+      // <div class="oac-card-table-cell">...</div>
+      child = child.firstChild;
+      expect(child.className).toContain(`oac-card-table-cell`);
+      
+      // <div class="oac-card-table-cell">
+      //   <div class="oac-card-cover"> ... </div>
+      //   <div class="oac-card-info"> ... </div>
+      // </div>
+      expect(child.children[0].className).toContain(`oac-card-cover`);
+      expect(child.children[1].className).toContain(`oac-card-info`);
     })
   })
 })
